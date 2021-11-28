58efa6eb550358ccae54590fc8dae6e0792ef01e
kpekala <kpekala@student.wszib.edu.pl>
2021-11-28 12:49:50 +0100

    update 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 70; i++) {
+        for (int i = 0; i < 77; i++) {
             System.out.println("Hello World!");
         }
 
